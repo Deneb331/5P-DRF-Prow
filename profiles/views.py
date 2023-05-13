453e82360c5ea8231d4fa9563fde23f12e1b8a37
Deneb331 <juanjo1.seriousmail@gmail.com>
2023-05-13 15:11:58 +0000

    Add permissions to edit profiles and authentication

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drow_drfapi.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -11,7 +12,10 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, 
+            many=True,
+            context={'request': request})
         return Response(serializer.data)
 
 
@@ -20,21 +24,29 @@ class ProfileDetail(APIView):
     List a single profile in detail and allows the user to edit it.
     """
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile,
+            context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, 
+            data=request.data,
+            context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
