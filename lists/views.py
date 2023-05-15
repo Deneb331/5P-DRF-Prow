3a6ca97b35ba09ce491821d2c6b794e86e358ad3
Deneb331 <juanjo1.seriousmail@gmail.com>
2023-05-15 19:16:39 +0000

    Add list detail view

--- lists/views.py
+++ lists/views.py
@@ -31,3 +31,44 @@ class ListList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ListDetail(APIView):
+    """
+    Detail view of a single list, edit form and delete option.
+    """
+    serializer_class = ListSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            listObj = List.objects.get(pk=pk)
+            self.check_object_permissions(self.request, listObj)
+            return listObj
+        except List.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        listObj = self.get_object(pk)
+        serializer = ListSerializer(
+            listObj,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        listObj = self.get_object(pk)
+        serializer = ListSerializer(
+            listObj,
+            data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        listObj = self.get_object(pk)
+        listObj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
