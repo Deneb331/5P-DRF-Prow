97d97c64dad4ae0b41945301a3ddb2ace89eb603
Deneb331 <juanjo1.seriousmail@gmail.com>
2023-06-27 13:19:33 +0000

    Fix bug in profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
-from cards.models import Card
 from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    card_list = serializers.SerializerMethodField()
     boards_count = serializers.ReadOnlyField()
     lists_count = serializers.ReadOnlyField()
     cards_count = serializers.ReadOnlyField()
@@ -15,19 +13,10 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_card_list(self, obj):
-        user = self.context['request'].user
-        card_list = Card.objects.filter(members=obj.owner)
-        if user.is_authenticated:
-            serialized_card_list = [card.title for card in card_list]
-            return serialized_card_list
-        return None
-
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
             'name', 'image', 'is_owner', 'workspaces', 
-            'card_list', 'boards_count', 'lists_count', 
-            'cards_count',
+            'boards_count', 'lists_count', 'cards_count',
         ]
